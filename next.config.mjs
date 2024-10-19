92cf44deb35f9f3946bb5317613871fbdc78ae7c
GW Oh <gameliker68@gmail.com>
2024-10-19 21:37:19 +0900

    이미지 출력수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,24 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+  images: {
+      domains: [
+          'www.notion.so',
+          'images.unsplash.com',
+          's3.us-west-2.amazonaws.com'
+      ],
+      format: ['image/png', 'image/webp', 'image/jpeg']
+    },
+    
     env: {
         NOTION_TOKEN: process.env.NOTION_TOKEN,
         NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
     },
+    
 };
 
 export default nextConfig;
+
+
+
